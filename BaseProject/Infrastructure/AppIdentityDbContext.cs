60afdf9235c73d5767e152c9084ccf0536a17dbb
Melis Archabaev <melis.archabaev.kg@gmail.com>
2024-02-01 18:54:39 +0600

    Initiated project

---
+++ BaseProject/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Infrastructure;
+
+public class AppIdentityDbContext : IdentityDbContext<AppUser>
+{
+    public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+    {
+    }
+}
